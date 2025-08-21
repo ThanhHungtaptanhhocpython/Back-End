9aaa68e08d1cd96bd1de5d9fd79760a10534087f
longnguyenha050 <22520965@gm.uit.edu.vn>
2025-08-22 02:44:16 +0700

    initial files

---
+++ src/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_cors import CORS
+import os
+from src.config.config import Config
+from dotenv import load_dotenv
+
+# loading environment variables
+load_dotenv()
+
+# declaring flask application
+app = Flask(__name__)
+CORS(app)
+
+# calling the dev configuration
+config = Config().dev_config
+
+# making our application to use dev env
+app.env = config.ENV
+
+
+from src.controllers.user_controller import users
+
+# register user with api blueprint
+app.register_blueprint(users, url_prefix="/users")
+
